60f15ef3dd22895011dc7c524056d53cc167297b
hfkimkak <furkankimkak@gmail.com>
2026-02-13 11:19:43 +0300

    fix: mongoose 9.x pre-save hook - remove next() callback

--- server/models/User.js
+++ server/models/User.js
@@ -27,17 +27,12 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-// Hash password before saving
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+// Hash password before saving (Mongoose 9.x: no next() in async hooks)
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Method to compare passwords
